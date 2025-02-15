5c105865a1a316bc52ff5b7a93157702d4aecbce
mugabiBenjamin <mugabiben6@gmail.com>
2025-02-15 18:56:25 +0300

    Added card deal section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@
 import PropTypes from 'prop-types';
 
 const Button = ({ styles }) => (
-  <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-black outline-none ${styles}`}>
+  <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-black outline-none ${styles} rounded-[10px]`}>
     Get Started
   </button>
 )
