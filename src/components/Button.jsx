38f815a6d3e46e567c26399c40733f7559cf10d0
mugabiBenjamin <mugabiben6@gmail.com>
2025-03-08 00:06:40 +0300

    Improve accessibility in Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,26 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ styles }) => (
-  <button type="button" className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-black outline-none ${styles} rounded-[10px]`}>
-    Get Started
+const Button = ({ styles, onClick, text }) => (
+  <button
+    type="button"
+    className={`py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-black outline-none ${styles} rounded-[10px] hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-[#33bbcf] transition-all`}
+    onClick={onClick}
+    aria-label={text}
+  >
+    {text}
   </button>
 )
 
-Button.propTypes = {  
-  styles: PropTypes.string,   // Expecting styles to be a string  
-};  
+Button.propTypes = {
+  styles: PropTypes.string,
+  onClick: PropTypes.func,
+  text: PropTypes.string,
+};
 
 Button.defaultProps = {
   styles: '',
+  onClick: () => { },
+  text: 'Get Started',
 };
 
 export default Button
