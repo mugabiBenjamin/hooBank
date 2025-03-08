25fe911a7d8fd921dd7435f3c614116486e1c97a
mugabiBenjamin <mugabiben6@gmail.com>
2025-03-08 12:38:14 +0300

    Adjust typo and responsiveness in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ import { footerLinks, socialMedia } from "../constants";
 const Footer = () => (
   <section className={`${styles.flexCenter} ${styles.paddingY} flex-col`}>
     <div className={`${styles.flexStart} md:flex-row flex-col mb-8 w-full`}>
-      <div className="flex1 flex flex-col justify-start mr-10">
+      <div className="flex-1 flex flex-col justify-start mr-10">
         <img src={logo} alt="hoobank" className="w-[266px] h-[72px] object-contain" />
         <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>
           A new way to make the payments easy, reliable and secure
@@ -14,7 +14,7 @@ const Footer = () => (
 
       <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
         {footerLinks.map((footerlink) => (
-          <div key={footerlink.key} className="flex flex-col sm:my-0 my-4 min-w-[150px]">
+          <div key={footerlink.title} className="flex flex-col sm:my-0 my-4 min-w-[150px]">
             <h4 className="font-medium text-[18px] leading-[27px] text-white">
               {footerlink.title}
             </h4>
@@ -30,7 +30,7 @@ const Footer = () => (
       </div>
     </div>
 
-    <div className="w-full flex justify-between item-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
+    <div className="w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]">
       <p className="font-normal text-center text-[18px] leading-[27px] text-white">
         &copy; 2021 HooBank. All Rights Reserved
       </p>
@@ -47,6 +47,6 @@ const Footer = () => (
       </div>
     </div>
   </section>
-)
+);
 
-export default Footer
+export default Footer;
