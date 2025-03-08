a6730c0048da4017339d42e367c6c774dd08a834
mugabiBenjamin <mugabiben6@gmail.com>
2025-03-08 11:12:46 +0300

    Replace lorem ipsum in  Billing.jsx

--- src/components/Billing.jsx
+++ src/components/Billing.jsx
@@ -11,7 +11,7 @@ const Billing = () => (
 
     <div className={layout.sectionInfo}>
       <h2 className={styles.heading2}>Easily control your billing <br className="sm:block hidden" /> and invoicing</h2>
-      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, velit itaque, asperiores recusandae alias quo odio accusantium ut reprehenderit nostrum officiis eligendi! Earum architecto totam adipisci magni sapiente, quae tempora?</p>
+      <p className={`${styles.paragraph} max-w-[470px] mt-5`}>Streamline your financial operations with our intuitive billing and invoicing system. Automate recurring payments, generate professional invoices, and gain real-time insights into your cash flow. Our secure platform helps you save time and reduce errors.</p>
 
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
         <img src={apple} alt="apple_store" className="w-[128px] h-[42px] object-contain mr-5 cursor-pointer" />
